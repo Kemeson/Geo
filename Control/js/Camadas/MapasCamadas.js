25b5bb48fc40c68817150fa75e6236cc609659fa
kemeson <kemeson123@gmail.com>
2024-01-10 17:04:03 -0400

    att

--- Control/js/Camadas/MapasCamadas.js
+++ Control/js/Camadas/MapasCamadas.js
@@ -24,9 +24,85 @@ var baseTree = {
               { label: 'Limites Municipais', layer: limitesMunic },
               { label: 'Rodovias', layer: rodovia },
         ]
+        }, {
+          label: '<b>Estabelecimentos de Saúde</b>',
+          collapsed: false,
+          children: [
+              { label: 'Estabelecimentos de Saúde', layer: markersUs },
+        ]
         },
     ]
   }
   
   
   var contr = L.control.layers.tree(baseTree, overlaysTree, {position: "topleft", collapsed: false}).addTo(map);
+/*
+
+  const legend = L.control.Legend({
+    position: "bottomleft",
+    collapsed: false,
+    symbolWidth: 24,
+    opacity: 1,
+    column: 2,
+    legends: [{
+        label: "Marker1",
+        type: "image",
+        url: "marker/marker-red.png",
+    }, {
+        label: "Marker2",
+        type: "image",
+        url: "marker/purple.png"
+    }, {
+        label: "Circle",
+        type: "circle",
+        radius: 6,
+        color: "blue",
+        fillColor: "#FF0000",
+        fillOpacity: 0.6,
+        weight: 2,
+        layers: [marker],
+        inactive: true,
+    }, {
+        label: "Real line",
+        type: "polyline",
+        color: "#FF0000",
+        fillColor: "#FF0000",
+        weight: 2,
+        layers: polyline
+    }, {
+        label: "Dotted line",
+        type: "polyline",
+        color: "#0000FF",
+        fillColor: "#0000FF",
+        dashArray: [5, 5],
+        weight: 2
+    }, {
+        label: "Rectangle",
+        type: "rectangle",
+        color: "#FF0000",
+        fillColor: "#FF0000",
+        weight: 2
+    }, {
+        label: "Square",
+        type: "polygon",
+        sides: 4,
+        color: "#FF0000",
+        fillColor: "#FF0000",
+        weight: 2
+    }, {
+        label: "Regular triangle",
+        type: "polygon",
+        sides: 3,
+        color: "#FF0000",
+        fillColor: "#FF0000",
+        weight: 2
+    }, {
+        label: "Regular polygon",
+        type: "polygon",
+        sides: 5,
+        color: "#FF0000",
+        fillColor: "#FF0000",
+        weight: 2
+    }]
+})
+.addTo(map);*/
---
+++ Control/js/Camadas/UnidadeSaude.js
@@ -0,0 +1,19 @@
+// Sedes Municipais
+
+var markersUs = L.markerClusterGroup();
+console.log(us1);
+
+var us = L.geoJSON(us1, {
+    pointToLayer: function (feature, latlng) {
+        switch (feature.properties.TP_GESTAO) {
+            case 'M': return L.marker(latlng, {icon: redIcon});
+            case 'E': return L.marker(latlng, {icon: VerdeIcon});
+            case 'D': return L.marker(latlng, {icon: CinzaIcon});
+        }
+    },
+    onEachFeature: function (geom, layer) {
+      layer.bindPopup('<h6><b>Estabelecimentos de Saúde</b></h6><p><b>Bairro:</b> '+geom.properties.NO_BAIRRO+'<br><b>Nome Fantasia:</b> '+geom.properties.NO_FANTASIA+'<br><b>Nome Logradouro:</b> '+geom.properties.NO_LOGRADOURO+'<br><b>Número Endereço:</b> '+geom.properties.NU_ENDERECO+'<br><b>TP_GESTAO:</b> '+geom.properties.TP_GESTAO+'</p>');
+    }
+  });
+
+  markersUs.addLayer(us);

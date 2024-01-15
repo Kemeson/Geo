d0e0142b700e53bd99aa7bd36b391ab12356128a
kemeson <kemeson123@gmail.com>
2024-01-15 16:34:25 -0400

    abrigos e ubs

--- Control/js/Camadas/PontMacro.js
+++ Control/js/Camadas/PontMacro.js
@@ -1,8 +1,15 @@
 var pontMacros1 = L.geoJSON(pontMacros, {
     pointToLayer: function (geom, latlng) {
-        return L.marker(latlng, {icon: RoxoIcon});
+        if(geom.properties.Name.includes('ABRIGO')){
+            return L.marker(latlng, {icon: RoxoIcon});
+        }else{
+            return L.marker(latlng, {icon: CinzaIcon});
+        }
+        
     },
     onEachFeature: function (geom, layer) {
     layer.bindPopup('<h6><b>UBS</b></h6><p><b>Nome:</b> '+geom.properties.Name+'</p>');
     }
 });
+
+console.log(pontMacros);
